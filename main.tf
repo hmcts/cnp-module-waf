e52499406d61d2700922af176241e58a6fe4a332
Liam F. O'Neill <liamfoneill@outlook.com>
2018-04-30 16:35:16 +0100

    Added Non-Mandatory Variables for Tagging

--- main.tf
+++ main.tf
@@ -4,23 +4,27 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.location}"
 }
 
-# The ARM template that creates a web app and app service plan
+# The ARM template that creates a Application Gateway, Public IP Address and Traffic Manager Profile
 data "template_file" "sitetemplate" {
   template = "${file("${path.module}/templates/waf.json")}"
 }
 
-# Create Application Service site
+# Create Application Gateway
 resource "azurerm_template_deployment" "waf" {
-  template_body       = "${data.template_file.sitetemplate.rendered}"
-  name                = "${var.product}-${var.env}"
-  resource_group_name = "${var.resourcegroupname}"
-  deployment_mode     = "Incremental"
-
+  template_body           = "${data.template_file.sitetemplate.rendered}"
+  name                    = "${var.product}-${var.env}"
+  resource_group_name     = "${var.resourcegroupname}"
+  deployment_mode         = "Incremental"
   parameters = {
-    name               = "${var.product}-${var.env}"
-    location           = "${var.location}"
-    virtualNetworkName = "${var.vnetname}"
-    subnetName         = "${var.subnetname}"
-    backendaddress     = "${var.backendaddress}"
+    name                  = "${var.product}-${var.env}"
+    location              = "${var.location}"
+    virtualNetworkName    = "${var.vnetname}"
+    subnetName            = "${var.subnetname}"
+    backendaddress        = "${var.backendaddress}"
+    team_name             = "${var.team_name}"
+    team_contact          = "${var.team_contact}"
+    env                   = "${var.env}"
+    destroy_me            = "${var.destroy_me}"
+
   }
 }
