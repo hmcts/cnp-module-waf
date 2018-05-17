2dc8cc3c54b5c3dd1975e16906e25cedfee11cc6
James Whinn <jameswhinn89@gmail.com>
2018-05-17 16:10:19 +0100

    try pass output as array

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "appGwIPs" {
-  value = "${azurerm_public_ip.appGwPIP-ukw.*.ip_address},${azurerm_public_ip.appGwPIP-uks.ip_address}"
+output "appGwUksIP" {
+  value = "${azurerm_public_ip.appGwPIP-uks.ip_address}"
+}
+
+output "appGwUkwIP" {
+  value = "${azurerm_public_ip.appGwPIP-ukw.*.ip_address}"
 }
