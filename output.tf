edac89c03706283744e31c262d2a93acc094a65f
James Whinn <jameswhinn89@gmail.com>
2018-05-17 15:29:31 +0100

    .

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "appGwUksIP" {
-  value = "${azurerm_public_ip.appGwPIP-uks.ip_address}"
+  value = "${azurerm_public_ip.appGwPIP-uks.*.ip_address}"
 }
 
 output "appGwUkwIP" {
-  value = "${azurerm_public_ip.appGwPIP-ukw.ip_address}"
+  value = "${azurerm_public_ip.appGwPIP-ukw.*.ip_address}"
 }
