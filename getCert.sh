28a4a02c97ed9000ae423f1eac7720c4d1551f61
Ben Hicks <bhicks329@gmail.com>
2018-07-20 10:54:53 +0100

    Updating script

--- getCert.sh
+++ getCert.sh
@@ -12,13 +12,36 @@ certName=$2
 fileLocation=$3
 subscription=$4
 
+azureConfigDir=/opt/jenkins/.azure-$subscription
+
 command="az keyvault certificate download --vault-name $vaultName --name $certName --file $fileLocation/$certName.cer"
 
 echo "Grabbing certificate"
 echo "Using $subscription"
-echo  
-result=$(env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription bash -e $command)
+echo ""
 
-echo $result
+if [ -d "$azureConfigDir" ]; then
+	echo "Config dir found for subscription $subscription"
+	echo "Grabbing certificate for $certName from $vaultName"
+	echo ""
+	result=$(env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription bash -e $command)
+else
+	echo "Config dir not found - running under current login"
+	echo "Grabbing certificate for $certName from $vaultName"
+	echo ""
+	result=$(bash -e $command)
+fi
 
-base64 -i $fileLocation/$certName.cer > $fileLocation/$certName.base64.out
+if [ -z "$result" ]; then
+	echo "Cert retrieved successfully ..."
+	echo ""
+	cat $fileLocation/$certName.cer
+	echo "Encoding file ..."
+	echo ""
+	base64 -i $fileLocation/$certName.cer >$fileLocation/$certName.base64.out
+	cat $fileLocation/$certName.base64.out
+else
+	echo "Error retrieving cert ...."
+	echo "$result"
+	exit
+fi
