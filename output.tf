655bf01c8b58ac94459a043dd9da2478a3891477
James Whinn <jameswhinn89@gmail.com>
2018-08-09 21:38:12 +0100

    revert to 6f5f63b

--- output.tf
+++ output.tf
@@ -2,6 +2,14 @@
 #   value = "${azurerm_template_deployment.waf.name}"
 # }
 
-output "appGwIP" {
-  value = "${azurerm_template_deployment.waf.outputs["appGwIP"]}"
-}
+# output "appGwIP" {
+#   value = "${azurerm_template_deployment.waf.outputs["appGwIP"]}"
+# }
+
+#   "outputs": {
+#     "appGwIP": {
+#       "type": "string",
+#       "value":
+#         "[reference(parameters('appGatewaySettings').frontendIPConfigurations[0].PublicIPName).publicIPAddress]"
+#     }
+#   }
