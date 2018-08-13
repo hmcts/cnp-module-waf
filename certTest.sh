fae9488a13515b6412f20f4c2c916a965cf4c896
James Whinn <jameswhinn89@gmail.com>
2018-08-14 00:07:19 +0200

    update params:

--- certTest.sh
+++ certTest.sh
@@ -5,6 +5,7 @@ set -e
 vaultName=${1}
 cert=${2}
 path=${3}
+subscription=${4}
 azureConfigDir=/opt/jenkins/.azure-$subscription
 
 certEntry=$(env AZURE_CONFIG_DIR=$azureConfigDir bash -e az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64)
