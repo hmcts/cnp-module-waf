25b814d76ecc96af6a06cbaff3411be4bc882396
James Whinn <jameswhinn89@gmail.com>
2018-08-13 22:56:36 +0200

    pass path

--- certTest.sh
+++ certTest.sh
@@ -4,9 +4,10 @@ set -e
 
 vaultName=${1}
 cert=${2}
+path=${3}
 
     certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 )
-    cat << EOF >> certs.json
+    cat << EOF >> ${3}/certs.json
     {
     name = "${cert}" 
     data = "${certEntry}"
