bf87a22248a02fc356aec8489c03aa16769c30b1
James Whinn <jameswhinn89@gmail.com>
2018-08-13 23:57:16 +0200

    update sh

--- certTest.sh
+++ certTest.sh
@@ -5,9 +5,10 @@ set -e
 vaultName=${1}
 cert=${2}
 path=${3}
+azureConfigDir=/opt/jenkins/.azure-$subscription
 
-certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64)
+certEntry=$(env AZURE_CONFIG_DIR=$azureConfigDir bash -e az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64)
 echo "command = az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 "
-echo "{name = \"${cert}\", data = \"${certEntry}\", password = \"${cert}\"}," >> ${3}/certs.json
+echo "{name = ${cert}, data = ${certEntry}, password = ${cert}}," >> ${3}/certs.json
 
 
