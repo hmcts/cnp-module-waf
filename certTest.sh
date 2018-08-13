519374172d24a8b8b1334dcf74b0ac4c9c2cab12
James Whinn <jameswhinn89@gmail.com>
2018-08-13 23:36:35 +0200

    updat sh

--- certTest.sh
+++ certTest.sh
@@ -8,12 +8,6 @@ path=${3}
 
 certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64)
 echo "command = az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 "
-cat << EOF >> ${3}/certs.json
-    {
-    name = "${cert}" 
-    data = "${certEntry}"
-    password = "${cert}"
-    },
-EOF
+echo "{name = \"${cert}\", data = \"${certEntry}\", password = \"${cert}\"}," >> certs.json
 
-cat ${3}/certs.json
+
