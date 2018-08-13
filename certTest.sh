a2a945568c741bf26e70cea35f203ca7f9d6f32b
James Whinn <jameswhinn89@gmail.com>
2018-08-13 23:18:48 +0200

    debvug

--- certTest.sh
+++ certTest.sh
@@ -6,7 +6,8 @@ vaultName=${1}
 cert=${2}
 path=${3}
 
-certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 )
+certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64)
+echo "command = az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 "
 cat << EOF >> ${3}/certs.json
     {
     name = "${cert}" 
