9f7a431279a07c867d6d6aaad947c6464a7876b7
James Whinn <jameswhinn89@gmail.com>
2018-08-13 23:02:14 +0200

    test eof

--- certTest.sh
+++ certTest.sh
@@ -6,10 +6,13 @@ vaultName=${1}
 cert=${2}
 path=${3}
 
-    certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 )
-    cat << EOF >> ${3}/certs.json
+certEntry=$(az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 )
+cat << EOF >> ${3}/certs.json
     {
     name = "${cert}" 
     data = "${certEntry}"
     password = "${cert}"
     },
+EOF
+
+cat ${3}/certs.json
