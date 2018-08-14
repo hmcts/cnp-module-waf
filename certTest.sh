99bf23f61b756961c18f85129564b7bc42e325df
James Whinn <jameswhinn89@gmail.com>
2018-08-14 13:48:37 +0200

    format certs.json

--- certTest.sh
+++ certTest.sh
@@ -10,7 +10,7 @@ azureConfigDir=/opt/jenkins/.azure-$subscription
 
 certEntry=$(env AZURE_CONFIG_DIR=$azureConfigDir bash -e az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -d | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64)
 echo "command = az keyvault secret show --vault-name ${vaultName} --name  ${cert} | jq -r .value | base64 -D | openssl pkcs12 -nodes -passin pass: | openssl pkcs12 -export -passout pass:${cert}  | base64 "
-echo "{name="${cert}",data="${certEntry}",password="${cert}"}," >> ${3}/certs.json
+echo "{name=\"${cert}\",data=\"${certEntry}\",password=\"${cert}\"}," >> ${3}/certs.json
 cat ${3}/certs.json | tr -d '\\n' >> ${3}/certsFormated.json
 
 
