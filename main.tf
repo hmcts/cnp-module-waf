555c53ce73da012ee4445ea1a8cb5b4f54ab794c
James Whinn <jameswhinn89@gmail.com>
2018-08-15 01:02:45 +0100

    get cert from vault

--- main.tf
+++ main.tf
@@ -238,7 +238,7 @@ resource "azurerm_template_deployment" "waf" {
     # The front HTTP listeners ports to be created on the WAF
     httpListeners = "${base64encode(jsonencode(var.httpListeners))}"
     # The SSL Certificates to be created on the WAF
-    sslCertificates = "${base64encode(jsonencode(data.local_file.certsList.content))}"
+    sslCertificates = "${base64encode(jsonencode(var.sslCertificates))}"
     # The backend address pools to be created on the WAF
     backendAddressPools = "${base64encode(jsonencode(var.backendAddressPools))}"
     # The http settings to be created on the WAF
@@ -254,18 +254,18 @@ resource "azurerm_template_deployment" "waf" {
   }
 }
 
-resource "null_resource" "sslCerts" {
-  triggers {
-      trigger = "${timestamp()}"
-      }
-  count = "${length(var.sslCertificates)}"
-  provisioner "local-exec" {
-    command = "bash -e  ${path.module}/certTest.sh infra-vault-${var.subscription} ${element(var.sslCertificates, count.index)} ${path.module} ${var.subscription}"
-  }
+# resource "null_resource" "sslCerts" {
+#   triggers {
+#       trigger = "${timestamp()}"
+#       }
+#   count = "${length(var.sslCertificates)}"
+#   provisioner "local-exec" {
+#     command = "bash -e  ${path.module}/certTest.sh infra-vault-${var.subscription} ${element(var.sslCertificates, count.index)} ${path.module} ${var.subscription}"
+#   }
 
-}
+# }
 
-data "local_file" "certsList" {
-    filename = "${path.module}/certs.json"
-    depends_on = ["null_resource.sslCerts"]
-}
+# data "local_file" "certsList" {
+#     filename = "${path.module}/certs.json"
+#     depends_on = ["null_resource.sslCerts"]
+# }
