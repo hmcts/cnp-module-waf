bc8df9cd1d84c5c29086f0355c6bff76be4885a0
Leonardo Fernandez Sanchez <leonfs@gmail.com>
2018-11-29 14:41:58 +0000

    Add ip fqdn output using data source (#21)
    * Outputing the FQDN of the public IP address

---
+++ output.tf
@@ -0,0 +1,10 @@
+data "azurerm_public_ip" "waf_public_ip" {
+  depends_on = ["azurerm_template_deployment.waf"]
+  
+  name                = "${lookup(local.frontendIPConfigurations[0], "publicIpName")}"
+  resource_group_name = "${var.resourcegroupname}"
+}
+
+output "public_ip_fqdn" {
+  value = "${data.azurerm_public_ip.waf_public_ip.fqdn}"
+}
